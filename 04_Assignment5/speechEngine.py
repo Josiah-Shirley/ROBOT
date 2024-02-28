c8dbf03b0eaad096290ef189e424bfccb1865d59
Josiah-Shirley <josiahfrommontana@gmail.com>
2024-02-28 15:53:45 -0700

    This is the first part of the assingment.

--- 04_Assignment5/speechEngine.py
+++ 04_Assignment5/speechEngine.py
@@ -1,1 +1,18 @@
+import speech_recognition as sr
 
+listening = True
+r = sr.Recognizer()
+
+while listening:
+    with sr.Microphone() as source:
+        # r.adjust_for_ambient_noise(source)
+        r.dynamic_energy_threshold = 300
+
+        try:
+            print("listening...")
+            audio = r.listen(source)
+            print("{processing audio...}")
+            word = r.recognize_google_cloud(audio)
+            print(word)
+        except sr.UnknownValueError:
+            print("..Unknown Word..")
