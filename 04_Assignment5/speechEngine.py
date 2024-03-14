71ba89fe9a1aebb43d29eafbb45de9b55637547e
Josiah-Shirley <josiahfrommontana@gmail.com>
2024-03-14 09:42:35 -0600

    Assignment 4 almost done.

--- 04_Assignment5/speechEngine.py
+++ 04_Assignment5/speechEngine.py
@@ -1,5 +1,8 @@
 import speech_recognition as sr
 import random
+import TTS
+import pyttsx3
+
 
 
 #----------------------------------------------------------------
@@ -7,12 +10,13 @@ import random
 
 
 def haveConversation():
-    dt = DialogueTemplate("dialogTestFile.txt")
+    dt = DialogueTemplate("huntersTestFile.txt")
     dt.interpretLines()
+    engine = pyttsx3.init()
     # dt.printPrimaryInputPairs()
 
     # Uncomment the following for final product...
-    
+    """
     listening = True
     r = sr.Recognizer()
 
@@ -20,7 +24,6 @@ def haveConversation():
         with sr.Microphone() as source:
             r.adjust_for_ambient_noise(source)
             r.dynamic_energy_threshold = 500
-
             try:
             # Listen for user input
                 print("listening...")
@@ -35,6 +38,7 @@ def haveConversation():
             except sr.UnknownValueError:
                 print("..Unknown Word..")
     """
+    
     # The following block of code is for testing without a micriphone
     listening = True
     while listening:
@@ -43,9 +47,10 @@ def haveConversation():
         if word == "stop":
             break
         response = dt.findResponse(word)
-        print(response)
+        engine.say(response)
+        engine.runAndWait()
     # End microphoneless testing block
-    """
+    
 
 class DialogueTemplate():
     def __init__(self, textFile):
@@ -66,53 +71,67 @@ class DialogueTemplate():
     def interpretLines(self):
         dialogueFile = open(self.textFile, "r")
         # "For each line in the dialogue text file..."
-        for line in dialogueFile:
-            # tokens = ['#', '~', 'u']
-            # "If it isn't a blank line..."
-            if line != "\n":
-                # "Take the first character in the line..."
-                firstChar = list(line)[0]
-                # "If that first charater is a tilda..."
-                if firstChar == "~":
-                    variableObject = Variable(line)
-                    self.variablesList.append(variableObject)
-                # "If that first character is a 'u' (Dont worry if it has a number or not just yet)..."
-                elif firstChar == "u":
-                    irp = ""
-                    hasUserInput = False
-                    for word in line:
-                        if word == "_":
-                            irp = InputResponsePairWithUserInfo(line)
-                            self.userInputObjectList.append(irp.getUserInfoObject())
-                            hasUserInput = True
-                    if not hasUserInput:    
-                        irp = InputResponsePair(line)
-                    # "Make an input response pair object with that line..."
-                    self.allInputPairs.append(irp)
-                    # "Get the second character in the line..."
-                    nextChar = list(line)[1]
-                    # "If it is a ':' it means that it is the highest level of possible user inputs
-                    # and should always be listened for..."
-                    if nextChar == ":":
-                        # "So add it to the primary input pair list (A list of these highest level
-                        # possible user inputs.)"
-                        self.primaryInputPairs.append(irp)
-                    # "Otherwise, it is some lower level of user input and should sometimes be listened for..."
+        for unProcessedline in dialogueFile:
+            try:
+                line = self.removeLeadingSpaces(unProcessedline)
+                # tokens = ['#', '~', 'u']
+                # "If it isn't a blank line..."
+                if line != "\n":
+                    # "Take the first character in the line..."
+                    firstChar = list(line)[0]
+                    # "If that first charater is a tilda..."
+                    if firstChar == "~":
+                        variableObject = Variable(line)
+                        self.variablesList.append(variableObject)
+                    # "If that first character is a 'u' (Dont worry if it has a number or not just yet)..."
+                    elif firstChar == "u":
+                        irp = ""
+                        hasUserInput = False
+                        for word in line:
+                            if word == "_":
+                                irp = InputResponsePairWithUserInfo(line)
+                                self.userInputObjectList.append(irp.getUserInfoObject())
+                                hasUserInput = True
+                        if not hasUserInput:    
+                            irp = InputResponsePair(line)
+                        # "Make an input response pair object with that line..."
+                        self.allInputPairs.append(irp)
+                        # "Get the second character in the line..."
+                        nextChar = list(line)[1]
+                        # "If it is a ':' it means that it is the highest level of possible user inputs
+                        # and should always be listened for..."
+                        if nextChar == ":":
+                            # "So add it to the primary input pair list (A list of these highest level
+                            # possible user inputs.)"
+                            self.primaryInputPairs.append(irp)
+                        # "Otherwise, it is some lower level of user input and should sometimes be listened for..."
+                        else:
+                            #print("Oh boy! Here comes a subpair!")
+                            pointer = self.primaryInputPairs[len(self.primaryInputPairs)-1]
+                            #print("pointer is currently pointing to:", pointer)
+                            for _ in range(int(nextChar)-1):
+                                pointer = pointer.getSubpairs()[len(pointer.getSubpairs())-1]
+                            #print("pointer is now pointing to:", pointer)
+                            #print("The IRP that was just added to pointer was:", irp)
+                            pointer.addSubpair(irp)
+                           #print("Pointer's subpairs are:")
+                    elif firstChar == "#":
+                        pass
                     else:
-                        #print("Oh boy! Here comes a subpair!")
-                        pointer = self.primaryInputPairs[len(self.primaryInputPairs)-1]
-                        #print("pointer is currently pointing to:", pointer)
-                        for _ in range(int(nextChar)-1):
-                            pointer = pointer.getSubpairs()[len(pointer.getSubpairs())-1]
-                        #print("pointer is now pointing to:", pointer)
-                        #print("The IRP that was just added to pointer was:", irp)
-                        pointer.addSubpair(irp)
-                        #print("Pointer's subpairs are:")
+                        print("This is a weird line...")
+                        print(line)
+            except Exception as e:
+                print("There was a problem reading this line:")
+                print(unProcessedline)
+                print(str(e))
         for pair in self.primaryInputPairs:
             self.activeUserInputPairs.append(pair)
         dialogueFile.close()
         for pair in self.allInputPairs:
             pair.setVariablesList(self.variablesList)
+
+    def removeLeadingSpaces(self, text):
+        return text.lstrip()
 
     def printPrimaryInputPairs(self):
         for pair in self.primaryInputPairs:
@@ -124,38 +143,77 @@ class DialogueTemplate():
 
     def findResponse(self, userInput) -> str:
         response = ""
+        print("Finding the right response...")
         foundResponse = False
         # Check all active pairs for a valid user input
         for pair in self.activeUserInputPairs:
+            # This will iterate through a list of possible inputs within the 
+            # current input-response pair being evaluated. The list will only
+            # contain one input unless there are variable inputs assigned to 
+            # the pair in the script.
             for input in pair.getPossibleInputs():
+                # GLOBAL VARIABLE: NO TOUCHY!
                 indexOfUnderscore = 0
+                # if an _ is in the possible input, it is a piece of user info
+                # and should be handled in a special way. If the user has entered
+                # a sentence that goes to an input with an _, it is handled here.
                 if "_" in input:
-                    currentIndex = 0
+                    # Also test if the entered input is the same length as the 
+                    # script input, because if they are not, we shouldn't waste time
+                    # checking for a match.
                     inputList = input.split(" ")
-                    for word in inputList:
-                        if word != "_":
-                            currentIndex += 1
-                        else:
-                            indexOfUnderscore = currentIndex
-                inputToTest = input.split(" ")
-                inputToTest.pop(indexOfUnderscore)
-                inputWithoutSpaces = "".join(inputToTest)
-                userInputToTest = userInput.split(" ")
-                possibleUserInputObjectValue = userInputToTest.pop(indexOfUnderscore)
-                userInputWithoutSpaces = "".join(userInputToTest)
-                if userInputWithoutSpaces == inputWithoutSpaces:
-                    userInputObjectName = ""
-                    possibleResponse = pair.getResponses()
-                    responseList = possibleResponse.split(" ")
-                    for word in responseList:
-                        if len(word) > 0:     
-                            if word[0] == "$":
-                                userInputObjectName = word[1:]
-                    self.setUserInfoObjectValue(userInputObjectName, possibleUserInputObjectValue)
-                    for pair in self.allInputPairs:
-                        pair.addUserInfoObjectValue(userInputObjectName, possibleUserInputObjectValue)
-                    response = pair.getResponses()
-                    foundResponse = True
+                    userInputList = userInput.split(" ")
+                    if len(inputList) == len(userInputList):
+                        # The following for loop and if/else statements find the index
+                        # position (assuming each word is a position) of the _
+                        currentIndex = 0
+                        for word in inputList:
+                            if word != "_":
+                                currentIndex += 1
+                            else:
+                                indexOfUnderscore = currentIndex
+                        # inputToTest is the predetermined input given in the script
+                        inputToTest = input.split(" ")
+                        # The idea of the following code is to take the underscore out
+                        # of the possible user input and then also take whatever word 
+                        # is at the same index position in the entered input. At that
+                        # point, the two can be compared for a match.
+                        inputToTest.pop(indexOfUnderscore)
+                        inputWithoutSpaces = "".join(inputToTest)
+                        # userInputToTest is the entered user input.
+                        possibleUserInputObjectValue = userInputList.pop(indexOfUnderscore)
+                        userInputWithoutSpaces = "".join(userInputList)
+                        if userInputWithoutSpaces == inputWithoutSpaces:
+                            userInputObjectName = ""
+                            possibleResponse = pair.getResponseWithVariableName()
+                            responseList = possibleResponse.split(" ")
+                            for word in responseList:
+                                # This quick check of the word's length protects against
+                                # a possible error that occurs when the script contains
+                                # two spaces next to eachother. The two spaces would result
+                                # in the value of 'word' being "" at some point during the
+                                # iteration of this for each loop, resuling in an index out
+                                # of range exception in the following if statement.
+                                if len(word) > 0:
+                                    if word[0] == "$":
+                                        userInputObjectName = word[1:]
+                            self.setUserInfoObjectValue(userInputObjectName, possibleUserInputObjectValue)
+                            for eachpair in self.allInputPairs:
+                                nameOfUserInfo = self.removeCharacterGroup(userInputObjectName, "\n")
+                                eachpair.addUserInfoObjectValue(nameOfUserInfo, possibleUserInputObjectValue)
+                            response = pair.getResponses()
+                            foundResponse = True
+                            # If the latest input is a primary input, deactivate all previous subpairs.
+                            if pair in self.primaryInputPairs:
+                                self.activeUserInputPairs.clear()
+                                for irp in self.primaryInputPairs:
+                                    self.activeUserInputPairs.append(irp)
+                            # Activate any subpairs of the current pair.
+                            #print("Here are all the subpairs for the current IRP:", pair.getSubpairs())
+                            subpairs = pair.getSubpairs()
+                            for subPair in subpairs:
+                                #print("Current subpair being added to active IRP list:", subPair)
+                                self.activeUserInputPairs.append(subPair)
             #print("Current pair being checked:", pair)
             #print("Here are all the subpairs for the current IRP:", pair.getSubpairs())
             subpairs = pair.getSubpairs()
@@ -176,7 +234,21 @@ class DialogueTemplate():
         if not foundResponse:
             response = "This does not appear to be an input I am prepared to handle..."
             response += "\n" + "'" + userInput + "'"
+            print("Could not find a valid response for this input...")
+        else:
+            print("Found one!")
         return response
+    
+    def removeCharacterGroup(self, word, group):
+        result = ''
+        i = 0
+        while i < len(word):
+            if word[i:i+2] == group:
+                i += 2
+            else:
+                result += word[i]
+                i += 1
+        return result
     
     def setUserInfoObjectValue(self, name, value) -> bool:
         for userInfoObject in self.userInputObjectList:
@@ -247,14 +319,17 @@ class InputResponsePair():
             variableObject = self.getVariableByName(self.responses[1:])
             toReturn = variableObject.getRandomValue()
         else:
-            responseList = self.responses.split(" ")
-            for word in responseList:
-                if word[0] == "$":
-                    key = word[1:]
-                    toReturn += self.userInfoDict[key]
-                else:
-                    toReturn += word
-                toReturn += " "
+            try:
+                responseList = self.responses.split(" ")
+                for word in responseList:
+                    if word[0] == "$":
+                        key = word[1:]
+                        toReturn += self.userInfoDict[key]
+                    else:
+                        toReturn += word
+                    toReturn += " "
+            except:
+                toReturn = "You have asked for user information that has not yet been defined..."
         return toReturn
     
     def getSubpairs(self):
@@ -297,16 +372,17 @@ class InputResponsePairWithUserInfo(InputResponsePair):
     def getUserInfoObject(self):
         return self.userInfoObject
     
+    def getResponseWithVariableName(self) -> str:
+        return self.responses
+
     def getResponses(self) -> str:
         toReturn = ""
-        if self.userInfoObject.getCurrentValue() == "":
-            toReturn = "\n" + "()()()This response contains a variable whose value has not been set yet()()()" + "\n"
-        else:
-            line = self.responses
-            lineList = line.split(" ")
-            for word in lineList:
+        responseWithVariable = self.responses
+        responseList = responseWithVariable.split(" ")
+        for word in responseList:
+            if len(word) > 0:
                 if word[0] == "$":
-                    toReturn += self.userInfoObject.getCurrentValue()
+                    toReturn += self.getUserInfoObject().getCurrentValue()
                 else:
                     toReturn += word
                 toReturn += " "
